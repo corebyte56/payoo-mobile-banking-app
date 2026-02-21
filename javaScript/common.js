5af63517c9b4691ebc461920a5a5e9b852d06494
corebyte56 <fahim789m@gmail.com>
2026-02-21 14:32:00 +0600

    feat: make index.html (login page) fully responsive for all devices

--- javaScript/common.js
+++ javaScript/common.js
@@ -13,6 +13,10 @@ function getValueOfInputItems(id) {
 }
 
 
+// This function takes an element id,
+// finds the innerText of that element,
+// and converts it into a Number before returning it.
+// Useful for getting balance, amount, etc. as a number.
 
 function getInnerTextOftItems(id) {
     const innerTextOfItems = document.getElementById(id).innerText;
@@ -20,13 +24,27 @@ function getInnerTextOftItems(id) {
 }
 
 
+
+// This function shows only the selected section
+// and hides all other sections (cashout, add-money, transfer-money, transaction).
+// After showing the selected section,
+// it automatically scrolls smoothly to that section.
+
 function showOnly(id){
     const cashOut = document.getElementById('cashout');
     const addMoney = document.getElementById('add-money');
+    const transferMoney = document.getElementById('transfer-money');
+    const transaction = document.getElementById('transaction');
     
     cashOut.classList.add('hidden');
     addMoney.classList.add('hidden')
+    transferMoney.classList.add('hidden')
+    transaction.classList.add('hidden')
 
     const selected = document.getElementById(id);
     selected.classList.remove("hidden");
+
+     selected.scrollIntoView({
+        behavior: "smooth"
+    });
 }
---
+++ javaScript/transfer.js
@@ -0,0 +1,39 @@
+document.getElementById('transfer-btn').addEventListener('click', function(e){
+    e.preventDefault()
+
+    const userNumber = getValueOfInputItems('user-mobile');
+    const transferAmount = getValueOfInputItems('transfer-amount');
+    const transferPin = getValueOfInputItems('transfer-pin');
+
+    const balance = getInnerTextOftItems('balance');
+
+    if(transferPin !== '1234' || transferPin.length !== 4 || userNumber.length !== 11){
+        alert("invalid info ");
+        return;
+    }
+
+    if(isNaN(transferAmount) || isNaN(userNumber)){
+    alert("Enter valid numbers");
+    return; 
+}
+
+
+
+    let newBalance = balance - parseFloat(transferAmount);
+
+    if(newBalance < 0){
+        alert('Insufficient balance')
+        return;
+    }
+
+    if(transferAmount <= 0){
+       alert('enter valid amount')
+        return;
+    }
+
+    console.log(newBalance)
+
+    document.getElementById('balance').innerText = newBalance;
+
+    alert("Transfer successful!");
+})
